27f5417535bf8e6c52608c7b3b13b38256b1dda6
aidinehsani1366 <57693993+aidinehsani1366@users.noreply.github.com>
2023-10-08 22:20:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, render_template, jsonify
 import tensorflow as tf
 import numpy as np
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
 import io
 
 app = Flask(__name__)
 
 # Load the MobileNetV2 model without weights (we will load custom weights)
-model = MobileNetV2(weights=None, include_top=True)
-model.load_weights('mobilenet_v2_weights_tf_dim_ordering_tf_kernels_1.0_224.h5')
+model = InceptionResNetV2(weights='imagenet')
+#model.load_weights('Inception-ResNet-v2.h5')
 
 @app.route('/')
 def home():
@@ -30,7 +30,7 @@ def predict():
         return jsonify({'error': 'Invalid file format'})
 
     # Load and preprocess the image for model prediction
-    img = image.load_img(io.BytesIO(image_file.read()), target_size=(224, 224))
+    img = image.load_img(io.BytesIO(image_file.read()), target_size=(299, 299))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
     img = preprocess_input(img)
